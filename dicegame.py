c2bb1d0b618708c59e7c337602e8d8f2f8663dee
aah120 <abdussamad2609@gmail.com>
2021-01-29 13:37:39 +0000

    Changed names of functions, variables and constants according to PEP8 conventions

--- dicegame.py
+++ dicegame.py
@@ -1,192 +1,217 @@
 import random
 
-ScorePlayer1 = 0
-ScorePlayer2 = 0
+SCORE_PLAYER1 = 0
+SCORE_PLAYER2 = 0
 
-Round = 1 
+round = 1
 N = 0
 
-def ShowTitle():
-  print("Welcome to Katarina's Dice Game.")
-  print("\n")
 
-def Player1Details():
-  NamePlayer1 = str(input("Player 1, please enter your name: "))
-  NamePlayer1 = NamePlayer1.capitalize()
-  print("Welcome,", NamePlayer1)
-  
-  return NamePlayer1
-
-def Player2Details():
-  NamePlayer2 = str(input("Player 2, please enter your name: "))
-  NamePlayer2 = NamePlayer2.capitalize()
-  print("Welcome,", NamePlayer2)
-  print("\n")
-  
-  return NamePlayer2
-
-def RoundDisplay(Round):
-  print("Please get ready for Round: ", Round)
-  print("\n")
-
-def Player1Rolling(NamePlayer1):
-  print(NamePlayer1, "get ready to roll your dice.")
-  Player1Input = str(input("Player 1, enter any key to roll your dice: "))
-
-  return Player1Input
-
-def Player1Roll1(NamePlayer1):
-  Player1FirstResult = random.randint(1,6)
-  print(NamePlayer1, "your first dice reads: ", Player1FirstResult)
-  
-  return Player1FirstResult
-
-def Player1Roll2(NamePlayer1):
-  Player1SecondResult = random.randint(1,6)
-  print(NamePlayer1, "your second dice reads: ", Player1SecondResult)
-  print("\n")
-
-  return Player1SecondResult
-
-def Player2Rolling(NamePlayer2):
-  print(NamePlayer2, "get ready to roll your dice.")
-  Player2Input = str(input("Player 2, enter any key to roll your dice: "))
-
-  return Player2Input
-
-def Player2Roll1(NamePlayer2):
-  Player2FirstResult = random.randint(1,6)
-  print(NamePlayer2, "your first dice reads: ", Player2FirstResult)
-
-  return Player2FirstResult
-
-def Player2Roll2(NamePlayer2):
-  Player2SecondResult = random.randint(1,6)
-  print(NamePlayer2, "your second dice reads: ", Player2SecondResult)
-  print("\n")
-
-  return Player2SecondResult
-
-def Player1CumulativeScore(NamePlayer1, Player1FirstResult, Player1SecondResult, Round):
-  Player1TotalResult = int(Player1FirstResult) + int(Player1SecondResult)
-  if Player1TotalResult % 2 == 0:
-    Player1Score = Player1TotalResult + 10
-    print(NamePlayer1, "your score after Round", Round, "is: ", Player1Score)
-  else:
-    Player1Score = Player1TotalResult - 5
-    print(NamePlayer1, "your score after Round", Round, "is: ", Player1Score)
-  
-  return Player1Score
-
-def Player2CumulativeScore(NamePlayer2, Player2FirstResult, Player2SecondResult, Round):
-  Player2TotalResult = int(Player2FirstResult) + int(Player2SecondResult)
-  if Player2TotalResult % 2 == 0:
-    Player2Score = Player2TotalResult + 10
-    print(NamePlayer2, "your score after Round", Round, "is: ", Player2Score)
-  else:
-    Player2Score = Player2TotalResult - 5
-    print(NamePlayer2, "your score after Round", Round, "is: ", Player2Score)
-  print("\n")
-
-  return Player2Score
-
-def WinnerDecider(NamePlayer1, NamePlayer2, Player1Score, Player2Score):
-  if Player1Score > Player2Score:
-    print("The winner of this game is: ", NamePlayer1)
-    Winner = NamePlayer1
-  elif Player1Score == Player2Score:
-    print("The game has ended in a draw. Proceed to the tiebreaker:")
+def show_title():
+    print("Welcome to Katarina's Dice Game.")
     print("\n")
-    TieBreaker(NamePlayer1, NamePlayer2, Player1Score, Player2Score)
-  else:
-    print("The winner of this game is: ", NamePlayer2)
-    Winner = NamePlayer2
-  print("\n")
 
-  return Winner
 
-def WinnerScoreCalculator(Winner, Player1Score, Player2Score):
-  if Winner == NamePlayer1:
-    WinnerScore = Player1Score
-  else:
-    WinnerScore = Player2Score
+def player1_details():
+    namePlayer1 = str(input("Player 1, please enter your name: "))
+    namePlayer1 = namePlayer1.capitalize()
+    print("Welcome,", namePlayer1)
 
-  return WinnerScore
+    return namePlayer1
 
-def TieBreaker(NamePlayer1, NamePlayer2, Player1Score, Player2Score):
-  print("Welcome to the tiebreaker.")
 
-  Player1Score = 0
-  Player2Score = 0
+def player2_details():
+    namePlayer2 = str(input("Player 2, please enter your name: "))
+    namePlayer2 = namePlayer2.capitalize()
+    print("Welcome,", namePlayer2)
+    print("\n")
 
-  Player1Input = str(input("Player 1, enter any key to roll your dice: "))
-  Player1TBRoll = random.randint(1,6)
-  Player2Input = str(input("Player 2, enter any key to roll your dice: "))
-  Player2TBRoll = random.randint(1,6)
+    return namePlayer2
 
-  print("Player 1, your tiebreaker dice reads: ", Player1TBRoll)
-  print("Player 2, your tiebreaker dice reads: ", Player2TBRoll) 
 
-  if Player1TBRoll % 2 == 0:
-    Player1Score = Player1TBRoll + 10
-    print("Player 1, your score for the tiebreaker is: ", Player1Score)
-  else:
-    Player1Score = Player1TBRoll - 5
-    print("Player 1, your score for the tiebreaker is: ", Player1Score)
-  
-  if Player2TBRoll % 2 == 0:
-    Player2Score = Player2TBRoll + 10
-    print("Player 2, your score for the tiebreaker is: ", Player2Score)
-  else:
-    Player2Score = Player2TBRoll - 5
-    print("Player 2, your score for the tiebreaker is: ", Player2Score)
-  print("\n")
+def round_display(round):
+    print("Please get ready for Round: ", round)
+    print("\n")
 
-  Winner = WinnerDecider(NamePlayer1, NamePlayer2, Player1Score, Player2Score)
 
-def SaveWinnerDetails(Winner, WinnerScore):
-  print("Saving Data...")
+def player1_rolling(namePlayer1):
+    print(namePlayer1, "get ready to roll your dice.")
+    player1Input = str(input("Player 1, enter any key to roll your dice: "))
 
-  file = open("WinnerDetails.txt", "a")
-  OutputData = Winner + "," + str(WinnerScore) + "\n"
-  file.write(OutputData)
-  file.close()
-  print("\n")
+    return player1Input
 
-def DisplayScoreboard():
-  file = open("WinnerDetails.txt", "r")
-  List = file.read()
-  print(List)
-  file.close()
+
+def player1_roll1(namePlayer1):
+    player1FirstResult = random.randint(1, 6)
+    print(namePlayer1, "your first dice reads: ", player1FirstResult)
+
+    return player1FirstResult
+
+
+def player1_roll2(namePlayer1):
+    player1SecondResult = random.randint(1, 6)
+    print(namePlayer1, "your second dice reads: ", player1SecondResult)
+    print("\n")
+
+    return player1SecondResult
+
+
+def player2_rolling(namePlayer2):
+    print(namePlayer2, "get ready to roll your dice.")
+    player2Input = str(input("Player 2, enter any key to roll your dice: "))
+
+    return player2Input
+
+
+def player2_roll1(namePlayer2):
+    player2FirstResult = random.randint(1, 6)
+    print(namePlayer2, "your first dice reads: ", player2FirstResult)
+
+    return player2FirstResult
+
+
+def player2_roll2(namePlayer2):
+    player2SecondResult = random.randint(1, 6)
+    print(namePlayer2, "your second dice reads: ", player2SecondResult)
+    print("\n")
+
+    return player2SecondResult
+
+
+def player1_cumulative_score(namePlayer1, player1FirstResult, player1SecondResult, round):
+    player1TotalResult = int(player1FirstResult) + int(player1SecondResult)
+    if player1TotalResult % 2 == 0:
+        player1Score = player1TotalResult + 10
+        print(namePlayer1, "your score after Round",
+              round, "is: ", player1Score)
+    else:
+        player1Score = player1TotalResult - 5
+        print(namePlayer1, "your score after Round",
+              round, "is: ", player1Score)
+
+    return player1Score
+
+
+def player2_cumulative_score(namePlayer2, player2FirstResult, player2SecondResult, round):
+    player2TotalResult = int(player2FirstResult) + int(player2SecondResult)
+    if player2TotalResult % 2 == 0:
+        player2Score = player2TotalResult + 10
+        print(namePlayer2, "your score after Round",
+              round, "is: ", player2Score)
+    else:
+        player2Score = player2TotalResult - 5
+        print(namePlayer2, "your score after Round",
+              round, "is: ", player2Score)
+    print("\n")
+
+    return player2Score
+
+
+def winner_decider(namePlayer1, namePlayer2, player1Score, player2Score):
+    if player1Score > player2Score:
+        print("The winner of this game is: ", namePlayer1)
+        winner = namePlayer1
+    elif player1Score == player2Score:
+        print("The game has ended in a draw. Proceed to the tiebreaker:")
+        print("\n")
+        tie_breaker(namePlayer1, namePlayer2, player1Score, player2Score)
+    else:
+        print("The winner of this game is: ", namePlayer2)
+        winner = namePlayer2
+    print("\n")
+
+    return winner
+
+
+def winner_score_calculator(winner, player1Score, player2Score):
+    if winner == namePlayer1:
+        winnerScore = player1Score
+    else:
+        winnerScore = player2Score
+
+    return winnerScore
+
+
+def tie_breaker(namePlayer1, namePlayer2, player1Score, player2Score):
+    print("Welcome to the tiebreaker.")
+
+    player1Score = 0
+    player2Score = 0
+
+    player1Input = str(input("Player 1, enter any key to roll your dice: "))
+    player1TBRoll = random.randint(1, 6)
+    player2Input = str(input("Player 2, enter any key to roll your dice: "))
+    player2TBRoll = random.randint(1, 6)
+
+    print("Player 1, your tiebreaker dice reads: ", player1TBRoll)
+    print("Player 2, your tiebreaker dice reads: ", player2TBRoll)
+
+    if player1TBRoll % 2 == 0:
+        player1Score = player1TBRoll + 10
+        print("Player 1, your score for the tiebreaker is: ", player1Score)
+    else:
+        player1Score = player1TBRoll - 5
+        print("Player 1, your score for the tiebreaker is: ", player1Score)
+
+    if player2TBRoll % 2 == 0:
+        player2Score = player2TBRoll + 10
+        print("Player 2, your score for the tiebreaker is: ", player2Score)
+    else:
+        player2Score = player2TBRoll - 5
+        print("Player 2, your score for the tiebreaker is: ", player2Score)
+    print("\n")
+
+    winner_decider(namePlayer1, namePlayer2, player1Score, player2Score)
+
+
+def save_winner_details(winner, winnerScore):
+    print("Saving Data...")
+
+    file = open("WinnerDetails.txt", "a")
+    outputData = winner + "," + str(winnerScore) + "\n"
+    file.write(outputData)
+    file.close()
+    print("\n")
+
+
+def display_scoreboard():
+    file = open("WinnerDetails.txt", "r")
+    list = file.read()
+    print(list)
+    file.close()
 
 #####################################################################
 
-ShowTitle()
-NamePlayer1 = Player1Details()
-NamePlayer2 = Player2Details()
+
+show_title()
+namePlayer1 = player1_details()
+namePlayer2 = player2_details()
 
 while N < 5:
-  RoundDisplay(Round)
+    round_display(round)
 
-  Player1Input = Player1Rolling(NamePlayer1)
-  Player1FirstResult = Player1Roll1(NamePlayer1)
-  Player1SecondResult = Player1Roll2(NamePlayer1)
+    player1Input = player1_rolling(namePlayer1)
+    player1FirstResult = player1_roll1(namePlayer1)
+    player1SecondResult = player1_roll2(namePlayer1)
 
-  Player2Input = Player2Rolling(NamePlayer2)
-  Player2FirstResult = Player2Roll1(NamePlayer2)
-  Player2SecondResult = Player2Roll2(NamePlayer2)
+    player2Input = player2_rolling(namePlayer2)
+    player2FirstResult = player2_roll1(namePlayer2)
+    player2SecondResult = player2_roll2(namePlayer2)
 
-  Player1Score = Player1CumulativeScore(NamePlayer1, Player1FirstResult, Player1SecondResult, Round)
-  Player2Score = Player2CumulativeScore(NamePlayer2, Player2FirstResult, Player2SecondResult, Round)
+    player1Score = player1_cumulative_score(
+        namePlayer1, player1FirstResult, player1SecondResult, round)
+    player2Score = player2_cumulative_score(
+        namePlayer2, player2FirstResult, player2SecondResult, round)
 
-  Round = Round + 1
-  N = N + 1
+    round = round + 1
+    N = N + 1
 
-if Player1Score != Player2Score:
-  Winner = WinnerDecider(NamePlayer1, NamePlayer2, Player1Score, Player2Score)
-  WinnerScore = WinnerScoreCalculator(Winner, Player1Score, Player2Score)
+if player1Score != player2Score:
+    winner = winner_decider(namePlayer1, namePlayer2,
+                            player1Score, player2Score)
+    winnerScore = winner_score_calculator(winner, player1Score, player2Score)
 else:
-  TieBreaker(NamePlayer1, NamePlayer2, Player1Score, Player2Score)
+    tie_breaker(namePlayer1, namePlayer2, player1Score, player2Score)
 
-SaveWinnerDetails(Winner, WinnerScore)
-DisplayScoreboard()
+save_winner_details(winner, winnerScore)
+display_scoreboard()
